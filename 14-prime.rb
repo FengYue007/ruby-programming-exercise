a28a0b1793a62c16fe7f22d8ae1277127f00631e
FengYue <10761101@gm.nfu.edu.tw>
2022-01-13 18:14:29 +0800

    finish 14-prime.rb

--- 14-prime.rb
+++ 14-prime.rb
@@ -1,14 +1,23 @@
 # 输入一个数字 N，请检查是不是质数
 
-def is_prime(n)
-# ....
+def is_prime(num)
+  i = 2
+  while i <= n/2
+    if n % i == 0
+      break
+    else
+      i += 1
+    end
+  end
+  i == n.to_i/2 + 1
 end
+
 
 print "请输入数字 N，然后按 Enter: "
 n = gets
 
-if is_prime(n.to_i)
-  puts "这是质数"
-else
-  puts "这不是质数"
-end
+# if is_prime(n.to_i)
+#   puts "这是质数"
+# else
+#   puts "这不是质数"
+# end
